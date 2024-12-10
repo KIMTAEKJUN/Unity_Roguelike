c50c25966652afb9560766578d0c5c62e9d00a80
TaekJun Kim <86834898+KIMTAEKJUN@users.noreply.github.com>
2024-12-10 22:06:53 +0900

    feat: 플레이어 변경 기능 구현

---
+++ Assets/Scripts/UI/HUD/PlayerSelectionUI.cs
@@ -0,0 +1,28 @@
+﻿using UI.Player;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI.HUD
+{
+    public class PlayerSelectionUI : MonoBehaviour
+    {
+        [SerializeField] private Button[] playerButtons;
+        [FormerlySerializedAs("playerSwitcher")] [SerializeField] private PlayerSwitcher switcher;
+
+        private void Start()
+        {
+            for (var i = 0; i < playerButtons.Length; i++)
+            {
+                var index = i; // Closure 방지
+                playerButtons[i].onClick.AddListener(() => SelectPlayer(index));
+            }
+        }
+
+        public void SelectPlayer(int index)
+        {
+            switcher.SwitchPlayer(index);
+            Debug.Log($"스킨 {index} 선택");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Player/PlayerSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UI.Camera;
+using UnityEngine;
+
+namespace UI.Player
+{
+    public class PlayerSwitcher : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] characterModels; // 캐릭터 모델 배열
+        private int _currentModelIndex = 0;
+        
+        [SerializeField] private CameraFollow cameraFollow;
+
+        public static event Action<GameObject> OnPlayerSwitched; // 플레이어 변경 이벤트
+
+        public void SwitchPlayer(int index)
+        {
+            if (index < 0 || index >= characterModels.Length)
+            {
+                Debug.LogError("유효하지 않은 캐릭터 모델 인덱스");
+                return;
+            }
+
+            // 이전 플레이어 비활성화
+            characterModels[_currentModelIndex].SetActive(false);
+
+            // 새로운 플레이어 활성화
+            _currentModelIndex = index;
+            var newPlayer = characterModels[_currentModelIndex];
+            newPlayer.SetActive(true);
+
+            if (cameraFollow != null)
+            {
+                cameraFollow.player = characterModels[_currentModelIndex].transform;
+            }
+            else
+            {
+                Debug.LogError("CameraFollow 스크립트가 연결되어 있지 않습니다.");
+            }
+
+            // 적들이 새로운 플레이어를 타겟으로 갱신
+            OnPlayerSwitched?.Invoke(newPlayer);
+
+            Debug.Log($"플레이어 변경: {newPlayer.name}");
+        }
+    }
+}
